89f8d353742e9aa8b65d3b56e2cc54a57209c125
misupopo <kokorononakaniitumo@yahoo.co.jp>
2016-11-10 19:15:39 +0900

    add local server of dummy

---
+++ local/js-flipsnap/example/demo.js
@@ -0,0 +1,28 @@
+$(function () {
+
+    if (!$('.demo').length) return;
+
+    (function simple() {
+        var flipsnap = Flipsnap('#demo-simple .flipsnap');
+
+        flipsnap.toNext();
+        flipsnap.toNext();
+        flipsnap.toNext();
+        flipsnap.toNext();
+        flipsnap.toNext();
+        flipsnap.toNext();
+        flipsnap.toNext();
+        flipsnap.toNext();
+        flipsnap.toNext();
+        flipsnap.toNext();
+        flipsnap.toNext();
+        flipsnap.toNext();
+        flipsnap.toNext();
+        flipsnap.toNext();
+        flipsnap.toNext();
+        flipsnap.toNext();
+        flipsnap.toNext();
+        flipsnap.toNext();
+        flipsnap.toNext();
+    })();
+});
---
+++ local/js-flipsnap/example/execute.js
@@ -0,0 +1,90 @@
+function execAction() {
+    function promiseAction () {
+        let target = $('.flipsnap'),
+            fireMouseEvent,
+            promiseActionNest,
+            elemDrag = target[0],
+            childTarget = test.children.length;
+
+        fireMouseEvent = function (type, elem, clientX, clientY, screenX, screenY) {
+            let evt = document.createEvent('MouseEvents');
+            evt.initMouseEvent(type, true, true, window, 1, screenX, screenY, clientX, clientY, false, false, false, false, 0, elem);
+            elem.dispatchEvent(evt);
+        };
+
+        promiseActionNest = function (count) {
+            return new Promise(function(resolve, reject) {
+                return setTimeout(function () {
+                    fireMouseEvent('mousedown', elemDrag, 134, 65, 156, 184);
+                    console.log('action1');
+                    return resolve();
+                }, 500);
+            }).then(function () {
+                return new Promise(function(resolve, reject) {
+                    setTimeout(function () {
+                        fireMouseEvent('mousemove', elemDrag, 354, 65, 356, 184);
+                        console.log('action2');
+                        return resolve();
+                    }, 500);
+                });
+            }).then(function () {
+                return new Promise(function(resolve, reject) {
+                    setTimeout(function () {
+                        fireMouseEvent('mousemove', elemDrag, 474, 65, 456, 184);
+                        console.log('action3');
+                        return resolve();
+                    }, 500);
+                });
+            }).then(function () {
+                return new Promise(function(resolve, reject) {
+                    setTimeout(function () {
+                        fireMouseEvent('mousemove', elemDrag, 584, 65, 556, 184);
+                        console.log('action4');
+                        return resolve();
+                    }, 500);
+                });
+            }).then(function () {
+                return new Promise(function(resolve, reject) {
+                    setTimeout(function () {
+                        fireMouseEvent('mouseup', elemDrag, 609, 65, 731, 184);
+                        console.log('action5');
+                        return resolve();
+                    }, 500);
+                });
+            }).then(function () {
+                console.log(count);
+
+                if(count < 2) {
+                    return;
+                }
+
+                promiseActionNest((count - 1));
+            });
+        };
+
+        let endCount = childTarget;
+
+        promiseActionNest(3);
+    }
+
+    promiseAction();
+}
+
+function getText(value) {
+    let text = value,
+        arrayData = [],
+        scriptData = '',
+        returnValue = '';
+
+    arrayData = text.split(/\r\n|\r|\n/);
+
+    for (let i = 0; i < arrayData.length; i++) {
+        scriptData = (scriptData + arrayData[i]);
+    }
+
+    returnValue = scriptData.replace(/\s+/g, ' ');
+
+    return returnValue;
+}
+
+
